f44898de06874925d28cbb90104065c390eab6c9
kartiknawal <kartiknawal.7@gmail.com>
2020-10-19 20:33:10 +0530

    [kartik] Add. initial commit

---
+++ LambdaExpressionsProject/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressionsProject
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public string Age;
+
+        public Person(string ssn, string name, string addr, string age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+
+    }
+}
